0b3e1441657f97fc980c9c611818d29cf492bf52
BetaniaJ2 <betania.ejimenez@gmail.com>
2019-06-21 22:46:20 -0600

    Identity

---
+++ SamSystem/SamSystemWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SamSystemWeb.Models;
+
+[assembly: HostingStartup(typeof(SamSystemWeb.Areas.Identity.IdentityHostingStartup))]
+namespace SamSystemWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SamSystemWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SamSystemWebContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<SamSystemWebContext>();
+            });
+        }
+    }
+}
